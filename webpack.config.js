0597d76dfec850eb3f7d0e14b6a0274c35580699
ijeomaese <ijeomaodinko@gmail.com>
2022-07-06 13:28:32 -0400

    add file

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// module.exports = {
-//   mode: "development",
-//   entry: "./src/index.js",
-//   output: {
-//     filename: "main.js",
-//     path: path.resolve(__dirname, "dist"),
-//   },
-// };
-
 module.exports = {
   mode: "development",
-  entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-      template: "./src/index.html",
-    }),
-  ],
+  entry: "./src/index.js",
   output: {
-    filename: "[name].bundle.js",
+    filename: "main.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  optimization: {
-    runtimeChunk: "single",
   },
 };
